f98a662fb785a7567eeb2e3444911b835fad07b7
Minh-Luan Tran <minhluan.tran3107@gmail.com>
2026-04-30 22:07:53 +0800

    Added new files

--- WD4E_Interactivity_with_Javascript/JS-week4-HW/js/gallery.js
+++ WD4E_Interactivity_with_Javascript/JS-week4-HW/js/gallery.js
@@ -1,29 +1,18 @@
-/*Name this external file gallery.js*/
+function setTabIndex(){
+        previewImages = document.querySelectorAll(".container img")
+        for (var i = 0; i < previewImages.length; i++) {
+            console.log("Image" + i);
+            previewImages[i].setAttribute("tabindex","0")
+        }
+    }
 
 function upDate(previewPic){
- /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-
     document.getElementById('image').style.backgroundImage = `url(${previewPic.src})`;
     document.getElementById('image').innerHTML = previewPic.alt;
-
 	}
 
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-	
+function unDo(){
     document.getElementById('image').style.backgroundImage = 'None';
     document.getElementById('image').innerHTML = "Hover over an image below to display here.";
-    
-	}
+    }
+
